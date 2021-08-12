cbb93354610436703e609821a7df068e9eb73d5d
Taras20191 <87118688+taras20191@users.noreply.github.com>
2021-08-12 11:05:59 +0300

    new

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Categories;
 use App\Models\News;
 use Illuminate\Routing\Controller;
-use Illuminate\Support\Facades\DB;
 
 class NewsController extends Controller
 {
@@ -16,11 +15,12 @@ class NewsController extends Controller
         return view('news', ['news' => $news, 'categories' => $categories]);
     }
 
-    public function category()
+    public function category($categories)
     {
-        $categories = DB::table('categories')
-            ->whereIn('id', [1, 2, 3])
-            ->get();
-        return view('news', ['categories' => $categories]);
+       $news = News::whereIn('id', function ($query) use ($categories) {
+            $query->select('news_id')->from('news_to_categories')->where('category_id');
+        })->get();
+        $categories = Categories::paginate(20);
+        return view('category_page', ['news' => $news, 'categories' => $categories]);
     }
 }
