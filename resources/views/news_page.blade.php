12a7a74d964105667bfbb295fff83fe62664f19a
Taras20191 <87118688+Taras20191@users.noreply.github.com>
2021-08-20 13:47:16 +0300

    new

--- resources/views/news_page.blade.php
+++ resources/views/news_page.blade.php
@@ -5,10 +5,14 @@
 @endsection
 @include('inc.header')
 @section('content')
+    <h2>Список категорий</h2>
     @foreach($categories as $element)
         <div>
             <a href="{{ route('category_page', ['category_id' => $element->id]) }}">{{$element->name}}</a>
         </div>
     @endforeach
-    <div style="word-break: break-all; font-size: 25px;">{{$news_info->news_description}}</div>
+    <h2 style="text-align: center;">
+        Новость
+    </h2>
+    <div style="word-break: break-all; font-size: 18px;">{{$news_info->news_description}}</div>
 @endsection
